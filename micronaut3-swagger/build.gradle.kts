47b7a5b5468be976c90db52404e9b30c4f2c87a5
Razvan Grecu <razvan.grecu@fxbits.io>
2025-03-18 11:49:30 +0200

    Add maven central publishing via plugin (will come back to do it manually later)

--- micronaut3-swagger/build.gradle.kts
+++ micronaut3-swagger/build.gradle.kts
@@ -1,18 +1,14 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    signing
     `java-library`
-    `maven-publish`
-    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish") version "0.31.0-rc2"
 }
 
 group = "dev.stephanson"
-description = "A library used with swagger codegen to provide older micronaut applications an out of the box solution for dealing with swagger."
+description =
+    "A library used with swagger codegen to provide older micronaut applications an out of the box solution for dealing with swagger."
 version = "0.0.1"
-
-java {
-    withSourcesJar()
-    withJavadocJar()
-}
 
 dependencies {
     implementation("io.swagger:swagger-codegen:2.4.41")
@@ -20,12 +16,34 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-publishing {
-    publications.create<MavenPublication>("local") {
-        artifactId = "micronaut3-swagger-generator"
-        groupId = project.group.toString()
-        version = project.version.toString()
+mavenPublishing {
+    coordinates(project.group.toString(), "micronaut3-swagger-generator", project.version.toString())
 
-        from(components["java"])
+    pom {
+        name.set("micronaut3-swagger-generator")
+        description.set(project.description)
+        url.set("https://github.com/stephan-song/micronaut-codegen")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set(providers.environmentVariable("DEVELOPER_ID"))
+                name.set(providers.environmentVariable("DEVELOPER_NAME"))
+                email.set(providers.environmentVariable("DEVELOPER_EMAIL"))
+            }
+        }
+        scm {
+            url.set("https://github.com/stephan-song/micronaut-codegen")
+            connection.set("scm:git://github.com:stephan-song/micronaut-codegen.git")
+            developerConnection.set("scm:git://github.com:stephan-song/micronaut-codegen.git")
+        }
     }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
+    signAllPublications()
 }
